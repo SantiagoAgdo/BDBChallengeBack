0f3ac451c1f62e862a978b27ae01459175b96012
santiago <santiago.agudelo@nuvu.cc>
2021-04-04 18:11:40 -0500

    project initializr

---
+++ AppLocation/src/test/java/com/BDB/AppLocation/AppLocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BDB.AppLocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppLocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
